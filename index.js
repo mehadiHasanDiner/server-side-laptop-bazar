b8d9532537345cefcfd9a9dc6cc2da8e90d46187
mehadi-500 <mehadi.cosmo@gmail.com>
2021-04-01 11:46:07 +0600

    home page server ready

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ctgcy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
